c65e66fdf500d46155081a7f3a31b855699afc6b
AyushInKC <sirfayush1008@gmail.com>
2025-12-06 16:07:59 +0530

    InterService Communication for Order and Inventory Service

--- inventory-service/src/main/java/com/Ayush/inventory_service/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/Ayush/inventory_service/repository/InventoryRepository.java
@@ -5,10 +5,9 @@ import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface InventoryRepository extends JpaRepository<Inventory,Long> {
-
-
-    Optional<Inventory> findBySkuCode(String skuCode);
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
 }
--- order-service/src/main/java/com/Ayush/order_service/service/OrderService.java
+++ order-service/src/main/java/com/Ayush/order_service/service/OrderService.java
@@ -2,13 +2,16 @@ package com.Ayush.order_service.service;
 
 import com.Ayush.order_service.dto.OrderLineItemsDto;
 import com.Ayush.order_service.dto.OrderRequest;
+import com.Ayush.order_service.model.InventoryResponse;
 import com.Ayush.order_service.model.Order;
 import com.Ayush.order_service.model.OrderLineItems;
 import com.Ayush.order_service.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -17,6 +20,7 @@ import java.util.UUID;
 @Transactional
 public class OrderService {
    private final OrderRepository orderRepository;
+   private final WebClient webClient;
     public void placeOrder(OrderRequest orderRequest){
         Order order=new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -26,7 +30,31 @@ public class OrderService {
                 .toList();
 
         order.setOrderLineItemsList(orderLineItems);
+
+        String skuCodes = String.valueOf(order.getOrderLineItemsList()
+                .stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList());
+
+
+        //Checking for the items in inventory if yes then place the order using WebClient for synchronous communication
+        InventoryResponse[] inventoryResponseArray=webClient.get()
+                .uri("http://localhost:8082/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+        //--> block() used for to make WebFlux synchronous
+
+        assert inventoryResponseArray != null;
+        boolean allProductsInStock=Arrays.stream(inventoryResponseArray).allMatch(InventoryResponse::isInStock);
+
+    if(allProductsInStock) {
         orderRepository.save(order);
+    }
+    else {
+        throw new IllegalArgumentException("Product is not in stock! Try again later.");
+    }
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
