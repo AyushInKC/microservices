4ae8043a2e92b4158f8700e68320562d26ab8f54
AyushInKC <sirfayush1008@gmail.com>
2025-12-07 12:29:29 +0530

    Implemented Eureka Server

--- product-service/src/main/java/com/Ayush/product_service/ProductServiceApplication.java
+++ product-service/src/main/java/com/Ayush/product_service/ProductServiceApplication.java
@@ -2,8 +2,9 @@ package com.Ayush.product_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.client.EnableEurekaClient;
 @SpringBootApplication
+@EnableEurekaClient
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
